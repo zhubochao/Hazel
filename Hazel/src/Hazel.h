f2ef859994a060488cc1d7f3951c25ff89bafab1
zhubochao <49826738+zhubochao@users.noreply.github.com>
2021-05-14 23:24:03 +0800

    Setup Basic Application and Entry Point

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+#include<stdio.h>
+
+//For use by Hazel Apps
+#include"Hazel/Application.h"
+
+//--Entry point---------
+#include"Hazel/EntryPoint.h"
+//---------------------
---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,12 @@
+#include "Application.h"
+
+namespace Hazel {
+	Application::Application(){}
+
+	Application::~Application() {}
+
+	void Application::Run() {
+		while (true);
+	}
+
+}
