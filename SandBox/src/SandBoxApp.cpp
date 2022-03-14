ade5e9617efa00327cdf4c22183866e8b012c3f8
zhubochao <921170528@qq.com>
2022-03-14 23:56:54 +0800

    Key Mouse Code and Math::glm

--- SandBox/src/SandBoxApp.cpp
+++ SandBox/src/SandBoxApp.cpp
@@ -1,16 +1,47 @@
 
 #include"Hazel.h"
+#include "glm/glm.hpp"
+
+#include <glm/gtc/type_ptr.hpp>
+
 
 class ExampleLayer : public Hazel::Layer
 {
 public:
-	ExampleLayer() : Layer("Example") {}
+	ExampleLayer() : Layer("Example") 
+	{
+		glm::mat4 trans(1.0f);
+		trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0, 1, 0));
+		//0  0  1  0
+		//0  1  0  0
+		//-1 0  0  0
+		//0  0  0  1
+		float* p = glm::value_ptr(trans);
+		std::cout << typeid(p).name() << std::endl;
+		for (int i = 0; i < 16; i++) {
+			std::cout << *p << ' ';
+			p++;
+		}
+		std::cout << std::endl;
+	}
 
-	void OnUpdate() override { HZ_INFO("ExampleLayer::Update"); }
+	void OnUpdate() override 
+	{
+		//HZ_INFO("ExampleLayer::Update"); 
+		if(Hazel::Input::IsKeyPressed(HZ_KEY_TAB))
+			HZ_TRACE("Tab is pressed(poll)");
+
+	}
 
 	void OnEvent(Hazel::Event& event) override 
 	{ 
-		HZ_TRACE("{0}", event); 
+		Hazel::KeyPressedEvent& e = (Hazel::KeyPressedEvent&)event;
+		if (event.GetEventType() == Hazel::EventType::KeyPressed)
+		{
+			HZ_TRACE("{0}", (char)e.GetKeyCode());
+			if (e.GetKeyCode() == HZ_KEY_TAB)
+				HZ_TRACE("Tab is pressed!(event)");
+		}
 	}
 	
 };
---
+++ src/hzpch.cpp
@@ -0,0 +1,5 @@
+#include "hzpch.h"
+
+
+
+
