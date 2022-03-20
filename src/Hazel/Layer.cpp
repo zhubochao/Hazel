db5981d4aaa9bb31af21774e5d12bdfcb25d746d
zhubochao <921170528@qq.com>
2022-03-20 22:08:21 +0800

    remove wrong files

--- src/Hazel/Layer.cpp
+++
@@ -1,8 +0,0 @@
-#include "hzpch.h"
-#include "Layer.h"
-
-namespace Hazel
-{
-	Layer::Layer(const std::string& name = "Layer") : m_DebugName(name) {}
-	Layer::~Layer() {}
-}
--- src/hzpch.cpp
+++
@@ -1,5 +0,0 @@
-#include "hzpch.h"
-
-
-
-
