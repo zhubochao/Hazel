183b15d84efb7ff58c0c34ae143e209578a7100f
zhubochao <921170528@qq.com>
2022-05-14 15:19:17 +0800

    Scene Hirearchy Panel

--- Hazelnut/src/EditorLayer.cpp
+++ Hazelnut/src/EditorLayer.cpp
@@ -67,6 +67,8 @@ namespace Hazel
 
 		m_CameraEntity.AddComponent<NativeScriptComponent>().Bind<CameraController>();
 		m_SecondCamera.AddComponent<NativeScriptComponent>().Bind<CameraController>();
+
+		m_SceneHierarchyPanel.SetContext(m_ActiveScene);
 	}
 
 	void EditorLayer::OnDetach()
@@ -167,6 +169,8 @@ namespace Hazel
 
 			ImGui::EndMenuBar();
 		}
+
+		m_SceneHierarchyPanel.OnImGuiRender();
 
 		ImGui::Begin("Settings");
 
--- Hazelnut/src/EditorLayer.h
+++ Hazelnut/src/EditorLayer.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "Hazel.h"
-
+#include "Panels/SceneHierarchyPanel.h"
 
 namespace Hazel
 {
@@ -39,6 +39,9 @@ namespace Hazel
 		glm::vec2 m_ViewportSize = { 0.0f, 0.0f };
 
 		glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
+
+		//Panel
+		SceneHierarchyPanel m_SceneHierarchyPanel;
 	};
 
 }
